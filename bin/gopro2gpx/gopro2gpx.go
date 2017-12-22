4839725e0bf745a19481d2d8c7a9698977d93db9
Laeyoung Chang <laeyoung.chang@alleys.co>
2017-12-22 17:46:52 +0900

    feat(gopro2gpx): change video's mTime to gpx's last timestamp

--- bin/gopro2gpx/gopro2gpx.go
+++ bin/gopro2gpx/gopro2gpx.go
@@ -14,9 +14,41 @@ import (
 func main() {
 	gpxData := new(gpx.GPX)
 
+	videoName := flag.String("v", "", "Required: video file to read timestamp")
 	inName := flag.String("i", "", "Required: telemetry file to read")
 	outName := flag.String("o", "", "Required: gpx file to write")
 	flag.Parse()
+
+
+	if *videoName == "" {
+		flag.Usage()
+		return
+	}
+
+	/*
+	// Opening the file handle
+	f, err := os.Open(*videoName)
+	if err != nil {
+		fmt.Printf("Cannot access video file %s.\n", *videoName)
+		return
+	}
+	defer f.Close()
+
+	// Checking if the opened handle is really a file
+	statinfo, err := f.Stat()
+	if err != nil {
+		fmt.Printf("stat() failure on %s.\n", *videoName)
+		return
+	}
+	duration := statinfo.ModTime().Unix()
+
+	fmt.Printf("duration: %d.\n", duration)
+	*/
+
+
+	lastTime := time.Now()
+	fmt.Printf("lastTime start: %s\n", lastTime)
+
 
 	if *inName == "" {
 		flag.Usage()
@@ -56,6 +88,7 @@ func main() {
 		t_prev.FillTimes(t.Time.Time)
 		telems := t_prev.ShitJson()
 
+
 		for i, _ := range telems {
 			segment.AppendPoint(
 				&gpx.GPXPoint{
@@ -67,11 +100,22 @@ func main() {
 					Timestamp: time.Unix(telems[i].TS/1000/1000, telems[i].TS%(1000*1000)),
 				},
 			)
+
+			//fmt.Printf("raw time: %s, %s\n", telems[i].TS/1000/1000, telems[i].TS%(1000*1000))
+			//fmt.Printf("Timestamp: %s\n", time.Unix(telems[i].TS/1000/1000, telems[i].TS%(1000*1000)))
+			lastTime = time.Unix(telems[i].TS/1000/1000, telems[i].TS%(1000*1000))
 		}
 
 		*t_prev = *t
 		t = &telemetry.TELEM{}
 	}
+
+	fmt.Printf("lastTime end: %s\n", lastTime)
+
+	if err := os.Chtimes(*videoName, time.Now(), lastTime); err != nil {
+		fmt.Printf("os.Chtimes() failure: %s.\n", err)
+	}
+
 
 	track.AppendSegment(segment)
 	gpxData.AppendTrack(track)
